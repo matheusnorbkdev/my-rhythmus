6cd4eb753a20724e064727e66363e9640c6d6213
matheusnorbkdev <matheus.nc10@aluno.ifsc.edu.br>
2026-03-23 14:15:31 -0300

    comentario

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
 import { Input } from "@/Components/Input"
 import { Button } from "@/Components/button"
 import Feather from '@expo/vector-icons/Feather'
-import { Link } from "expo-router"
+import { Link, useRouter } from "expo-router"
 import { useState } from "react"
-import {useRouter} from "expo-router"
-import { Alert, Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 export default function Index(){
+	const [senha, setSenha] = useState("")
+	
 	const [viewPassNormie, setViewPassNormie] = useState(true)
 	const router = useRouter()
     return (
@@ -15,23 +16,27 @@ export default function Index(){
   enableOnAndroid={true}
   extraScrollHeight={20}
   keyboardShouldPersistTaps="handled"
->
+>	
+		{/*IMAGEM e configurações */}
 		 <View>
 			<Image source={require("@/assets/init.png")}
 			style = {styles.illustration}
 			/>
-			<Text style = {styles.title}>Entrar</Text>
-			<Text style = {styles.subtitle}>Clique aqui para conseguir entrar em sua conta</Text>
-		<View style = {styles.form}>
-			<Input placeholder="E-mail" keyboardType="email-address" onChangeText = {(text) => console.log(text)} />
-			{/* */}
-			<View style={styles.containerInPass}>
+		{/*botão de entrar*/}	
+				<Text style = {styles.title}>Entrar</Text>
+				<Text style = {styles.subtitle}>Clique aqui para conseguir entrar em sua conta</Text>
+			<View style = {styles.form}>
+				<Input placeholder="E-mail" keyboardType="email-address" onChangeText = {(text) => console.log(text)} />
+				{/* Parte da senha*/}
+				<View style={styles.containerInPass}>
                         <Input
-                            placeholder="Confirme sua senha"
+                            placeholder="Senha:"
                             secureTextEntry={viewPassNormie}
                             style={styles.inPass}
+							value={senha}
+							onChangeText={setSenha}
                         />
-
+						{/* botão para mostrar/esconder senha */}
                         <Pressable style={styles.btnEyePass} onPress={() => setViewPassNormie(!viewPassNormie)}>
                             <Feather
                                 name={viewPassNormie ? "eye" : "eye-off"}
@@ -40,73 +45,76 @@ export default function Index(){
                             />
                         </Pressable>
                     </View>
-			
+			{/* botão de entrar, aqui tem um onPress que é pra ir pra home, depois a gente muda isso pra ir pra tela de autenticação, mas por enquanto vamo deixar assim pra testar a home e o layout */}
 			<Button label ="Entrar" onPress={() =>router.push("/tabs/home")}/>
 		</View>
+		{/* Link pro cadastro*/}
 		<Text style= {styles.footerText}>
 			Nao tem uma conta?
 			<Link style = {styles.link} href="/signup"> Cadastre-se aqui! </Link> 
 		</Text>
-		{/* aqui tem um link para a home, parte de desenvolvedor, obviamente vamo tira depois, aqui nao tek mais, se nao erra saporra */}
 	</View>
 	</KeyboardAwareScrollView>
 
     )
 }
 const styles = StyleSheet.create({
-	illustration: {
-		flex : 1,
-		width : "100%",
-		height: 200,
-		resizeMode : "contain",
-		marginTop : 32,
-		marginBottom : 22,
-	},
-	container: {
-	flex: 1,
-	backgroundColor : "#fff",
-	padding : 32,
-	},
-	title: {
-	fontSize: 32,
-	fontWeight: "900",
-	textAlign : "center",
-	marginBottom : 8,	
-	},
-	subtitle: {
-		fontSize: 16,
-	},
-	image: {
-	width: "100%",
-	height: "100%",
-	resizeMode: "cover",
-	},
-	form: {
-		marginTop : 24,
-		gap: 12,
-	},
-	footerText: {
-		fontSize: 14,
-		textAlign: "center",
-		marginTop: 24,
-		color: "#585860",
-	},
-	link: {
-		color: "#2772c9",
-		textDecorationLine: "underline",
-	},
-	containerInPass: {
+    //ilustration é a imagem do cadastro, tem um flex 1 pra ela ocupar todo o espaço disponivel, 
+    // width 100% pra ela ocupar toda a largura da tela, height 200 pra deixar ela com uma altura fixa, 
+    // resizeMode contain pra ela manter a proporção da imagem, marginTop 32 pra deixar um espacinho entre o topo da tela e a imagem
+    illustration:{
+        flex:1,
+        width:"100%",
+        height:200,
+        resizeMode:"contain",
+        marginTop: 32,
+    },
+    //container serve para deixar o fundo branco, e colocar um padding de 32 em toda a tela
+    container:{
+        flex:1,
+        backgroundColor:"#FDFDFD",
+        padding: 16,
+    },
+    //titulo
+    title:{
+        fontSize:32,
+        fontWeight:"900",
+        textAlign:"center",
+    },
+    //subtitulo
+    subtitle:{
+        fontSize:16,
+    },
+    //form serve para deixar os inputs do cadastro com um gap de 10, gap é a distancia entre os elementos, e marginTop 14 é pra deixar um espacinho entre o subtitulo e os inputs
+    form:{
+        marginTop:14,
+        gap:10,
+    },
+    //texto do rodapé, tem um link pra ir pra tela de login, o link é do expo-router, e tem um estilo pra deixar ele azul e sublinhado
+    footerText:{
+        fontSize:14,
+        textAlign:"center",
+        marginTop:24,
+        color:"#585860",
+    },
+    //serve para deixar o input de senha e o botão de mostrar senha alinhados, o flexDirection row é pra deixar eles na horizontal, e o alignItems center é pra deixar eles alinhados verticalmente
+    containerInPass:{
         flexDirection:"row",
         alignItems:"center",
     },
-
+    //deixa o botao do olho de senha 12 pra direita, 8 pra baixo, e deixa ele em cima do input de senha com o position absolute
     btnEyePass:{
         position:"absolute",
         right:12,
+        marginBottom: 8,
     },
-
+    // é pra fazer o input de senha ficar bonitinho, sem isso o input de senha fica gigante
     inPass:{
         flex:1,
     },
+    //link
+    link:{
+        color:"#2772c9",
+        textDecorationLine:"underline",
+    }
 })
-
--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,4 +1,4 @@
-import { Image, Alert, ScrollView, StyleSheet, Text, View, Pressable, K} from "react-native"
+import { Image, Alert, ScrollView, StyleSheet, Text, View, Pressable} from "react-native"
 import { Button } from "@/Components/button"
 import { Input } from "@/Components/Input"
 import { Link } from "expo-router"
@@ -100,7 +100,9 @@ export default function Signup(){
 }
 
 const styles = StyleSheet.create({
-
+    //ilustration é a imagem do cadastro, tem um flex 1 pra ela ocupar todo o espaço disponivel, 
+    // width 100% pra ela ocupar toda a largura da tela, height 200 pra deixar ela com uma altura fixa, 
+    // resizeMode contain pra ela manter a proporção da imagem, marginTop 32 pra deixar um espacinho entre o topo da tela e a imagem
     illustration:{
         flex:1,
         width:"100%",
@@ -108,49 +110,50 @@ const styles = StyleSheet.create({
         resizeMode:"contain",
         marginTop: 32,
     },
-
+    //container serve para deixar o fundo branco, e colocar um padding de 32 em toda a tela
     container:{
         flex:1,
         backgroundColor:"#FDFDFD",
         padding: 16,
     },
-
+    //titulo
     title:{
         fontSize:32,
         fontWeight:"900",
         textAlign:"center",
     },
-
+    //subtitulo
     subtitle:{
         fontSize:16,
     },
-
+    //form serve para deixar os inputs do cadastro com um gap de 10, gap é a distancia entre os elementos, e marginTop 14 é pra deixar um espacinho entre o subtitulo e os inputs
     form:{
         marginTop:14,
         gap:10,
     },
-
+    //texto do rodapé, tem um link pra ir pra tela de login, o link é do expo-router, e tem um estilo pra deixar ele azul e sublinhado
     footerText:{
         fontSize:14,
         textAlign:"center",
         marginTop:24,
         color:"#585860",
     },
-
+    //serve para deixar o input de senha e o botão de mostrar senha alinhados, o flexDirection row é pra deixar eles na horizontal, e o alignItems center é pra deixar eles alinhados verticalmente
     containerInPass:{
         flexDirection:"row",
         alignItems:"center",
     },
-
+    //deixa o botao do olho de senha 12 pra direita, 8 pra baixo, e deixa ele em cima do input de senha com o position absolute
     btnEyePass:{
         position:"absolute",
         right:12,
+        marginBottom: 8,
     },
-
+    // é pra fazer o input de senha ficar bonitinho, sem isso o input de senha fica gigante
     inPass:{
         flex:1,
     },
-
+    //link
     link:{
         color:"#2772c9",
         textDecorationLine:"underline",
